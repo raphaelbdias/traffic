2a0b576bc37a67717857af8262bf13f5f913b477
ChristianTrojas <138070305+ChristianTrojas@users.noreply.github.com>
2023-07-01 21:13:34 +0000

    Madarchod-Hijodeputa

--- cars_static.py
+++ cars_static.py
@@ -10,3 +10,22 @@ logic flow:
     - randomly chooce a point for each each car in "TrafficData"
     
 """
+import csv
+
+def find_highway_length(csv_file, highway_name):
+    with open(csv_file, 'r') as file:
+        reader = csv.DictReader(file)
+        total_length = 0
+        
+        for row in reader:
+            if row['name'] == highway_name:
+                length = float(row['length'])
+                total_length += length
+        
+        return total_length
+
+# Example usage
+csv_file = 'TrafficData.csv'
+highway_name = 'Highway 144'
+total_length = find_highway_length(csv_file, highway_name)
+print(f"The total length of {highway_name} is: {total_length} units")
